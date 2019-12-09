8695acc3101b31e40f8e0172809d457200edf84d
Aleksandr Davidenko <alex1190119022@gmail.com>
2019-12-09 20:28:01 +0200

    Добавьте файлы проекта.

---
+++ LinkedList/Model/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LinkedList.Model
+{
+    public class Item<T>
+    {     /// <summary>
+          /// Хранимые данные.
+          /// </summary>
+        public T Data = default(T);
+
+        /// <summary>
+        /// Следующий элемент связного списка.
+        /// </summary>
+        public Item<T> Next { get; set; }
+        /// <summary>
+        /// Создание нового экземпляра связного списка.
+        /// </summary>
+        /// <param name="data"> Сохраняемые данные. </param>
+        public Item(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            Data = data;
+        }
+        /// Приведение объекта к строке
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
